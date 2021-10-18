101ffebd513d5b842f2364aa66ef118748c46449
moondial <lp@luispal.com>
2021-10-17 20:52:40 -0700

    added apps in setting.py for the 3 main views. fixed #3, fixed #4, fixed #5. need to add path rules for clients & schedule.

--- crew/crew/settings.py
+++ crew/crew/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.dashboard',
+    'django.contrib.clients',
+    'django.contrib.schedule',
 ]
 
 MIDDLEWARE = [
