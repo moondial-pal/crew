8eeed079817694fb1071e454650f7ebcdd8dc200
Michael Abrahamsen <mail@michaelabrahamsen.com>
2021-10-20 08:25:21 -0600

    initial project structure

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-"""crew URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib import dashboard
-from django.contrib import clients
-from django.contrib import schedule
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard', views.dashboard),
-    path('clients', views.clients),
-    path('schedule', views.schedule),
 ]
