34e6056af93a6c5b06dab8fb36d483b9984f9638
Michael Abrahamsen <mail@michaelabrahamsen.com>
2021-10-27 08:44:09 -0600

    display list of clients on index

--- crew/views.py
+++ crew/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from crew.models import Client
 
 # Create your views here.
 
 
 def index(request):
+    # get a list of all clients
+    clients = Client.objects.all()
+
     return render(
         request, "index.html",
         {
             "test_var": "hello world",
+            "clients": clients
         },
     )
