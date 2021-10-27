28480aea9a286c76922617313cdaca97a7ca7613
Michael Abrahamsen <mail@michaelabrahamsen.com>
2021-10-27 08:43:30 -0600

    create admin for Client
    - rename client to Client

--- crew/models.py
+++ crew/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
 
-class client(models.Model):
+class Client(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = PhoneNumberField(null=False, blank=False, unique=True)
     first_service_date = models.DateField()
-    services_per_month= models.CharField(max_length=100)
+
+    # change to integer field
+    services_per_month = models.CharField(max_length=100)
+    # change to integer field
     service_duration = models.CharField(max_length=100)
+    # make optional
     images = models.ImageField()
+    # make optional
     notes = models.CharField(max_length=2000)
