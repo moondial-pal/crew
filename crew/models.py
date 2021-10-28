252501df63a0f9234054b7f033be9d8fd618da71
moondial <lp@luispal.com>
2021-10-28 07:31:02 -0700

    adjusted so that images are saved in app dir

--- crew/models.py
+++ crew/models.py
@@ -11,5 +11,5 @@ class Client(models.Model):
     first_service_date = models.DateField()
     services_per_month = models.IntegerField()
     service_duration = models.IntegerField()
-    images = models.ImageField(blank=True, upload_to='media/images/')
+    images = models.ImageField(blank=True, upload_to='crew/media/images/')
     notes = models.CharField(blank=True, max_length=2000)
