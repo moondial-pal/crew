186113e8559e97e96bc1815cb269d25f5a3097df
moondial <lp@luispal.com>
2021-10-27 20:34:35 -0700

    fixed #34, fixed #35, fixed #36 - updated client model fields.

--- crew/models.py
+++ crew/models.py
@@ -2,19 +2,14 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class Client(models.Model):
+class Client(models.Model): 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = PhoneNumberField(null=False, blank=False, unique=True)
     first_service_date = models.DateField()
-
-    # change to integer field
-    services_per_month = models.CharField(max_length=100)
-    # change to integer field
-    service_duration = models.CharField(max_length=100)
-    # make optional
-    images = models.ImageField()
-    # make optional
-    notes = models.CharField(max_length=2000)
+    services_per_month = models.IntegerField(max_length=100)
+    service_duration = models.IntegerField(max_length=100)
+    images = models.ImageField(blank=True)
+    notes = models.CharField(blank=True, max_length=2000)
