6b27da318ffc4c909b17d436b5a5c71e04b00510
moondial <lp@luispal.com>
2022-01-04 19:17:10 -0800

    need to figure out how to get img rendered on page

--- crew/forms.py
+++ crew/forms.py
@@ -1,4 +1,8 @@
 from address.forms import AddressField
 from django import forms
 class PersonForm(forms.Form):
-  address = AddressField()
+    address = AddressField()
+
+
+class NotesForm(forms.Form):
+    comment = forms.CharField(widget=forms.Textarea)
